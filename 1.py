5f7a512f690c80ac666935fe6bc0d4d1dd3365e1
Fedoua <fedouqdib119@gmail.com>
2025-03-11 21:13:30 +0100

    Initial commit: Add two Python files

--- 1.py
+++ 1.py
@@ -2,6 +2,7 @@ firstName = input("Enter your first name: ")
 lastName = input("Enter your last name: ")
 
 if firstName and lastName:
-    print("Welcome, Fedoua Dib! ")
+    print(f"Welcome, {firstName} {lastName}!")
+
 else:
     print("Error: Both first name and last name must be provided: ")
