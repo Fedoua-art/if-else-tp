bcb3b6dd6a1568baf2237908ccfd069812679c80
Fedoua <fedouqdib119@gmail.com>
2025-03-11 20:08:41 +0100

    Initial commit: Add two Python files

---
+++ 1.py
@@ -0,0 +1,7 @@
+firstName = input("Enter your first name: ")
+lastName = input("Enter your last name: ")
+
+if firstName and lastName:
+    print("Welcome, Fedoua Dib! ")
+else:
+    print("Error: Both first name and last name must be provided: ")
---
+++ 2.py
@@ -0,0 +1,9 @@
+username = input("Enter your username: ")
+password = input("Enter your password: ")
+
+if username ==  "Fedoua1004" and password == "V3012":
+    print("Login successful: ")
+elif username == "Fedoua1004":
+    print(" Invalid username: ")
+else:
+    print(" Invalid password: ")
